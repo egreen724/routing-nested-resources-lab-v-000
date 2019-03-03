079160288793cf471d8b06a40c21607bccfc509b
Erin Green <erin.y.green@gmail.com>
2019-03-03 19:33:00 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,6 +16,7 @@ class SongsController < ApplicationController
   def show 
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
       if Song.find_by(id: params[:id]).nil? 
         flash[:alert] = "Song not found."
         redirect_to artist_songs_path(@artist)
